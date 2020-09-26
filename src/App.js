ebc0975f584e6f50878f30f5e63a55168b370065
TobiasLab <tobias.labidi@gmail.com>
2020-09-27 00:51:58 +0200

    Lots of small fixes for the structure of the project. Removed default react page and replaced with the actual landing page as well as a navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import React from 'react';
-import logo from './logo.svg';
-import './css/App.css';
+import { Navbar } from 'react-bootstrap';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Router>
+
+            <Navbar bg="dark" variant="dark"  className="mb-5">
+                <Navbar.Brand> Consulting</Navbar.Brand>
+            </Navbar>
+
+        </Router>
+    </>
   );
 }
 
