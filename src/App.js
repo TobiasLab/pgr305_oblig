e0286a9be8f1b9f61bbe4deca9715b37a931b725
TobiasLab <tobias.labidi@gmail.com>
2020-09-27 19:41:51 +0200

    More links and routes in navbar as well as a landing page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import Customers from './components/Customers';
+import Employees from './components/Employees';
+import Projects from './components/Projects';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
 
@@ -12,7 +19,26 @@ function App() {
 
             <Navbar bg="dark" variant="dark"  className="mb-5">
                 <Navbar.Brand> Consulting</Navbar.Brand>
+                <Nav>
+                    <Nav.Link as={Link} to="/">Hjem</Nav.Link>
+                    <Nav.Link as={Link} to="/customers">Customers</Nav.Link>
+                    <Nav.Link as={Link} to="/employees">Employees</Nav.Link>
+                    <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+                    <Nav.Link as={Link} to="/about">About us</Nav.Link>
+                </Nav>
             </Navbar>
+
+            <Container>
+                <main>
+                    <Switch>
+                        <Route exact path="/" component={Home}></Route>
+                        <Route exact path="/customers" component={Customers}></Route>
+                        <Route exact path="/employees" component={Employees}></Route>
+                        <Route exact path="/projects" component={Projects}></Route>
+                        <Route exact path="/about" component={About}></Route>
+                    </Switch>
+                </main>
+            </Container>
 
         </Router>
     </>
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Home = () => {
+    return (
+        <Row>
+            <Col>
+                <section>
+                    <h3>I don't know what to write here yet lmao</h3>
+                </section>
+            </Col>
+        </Row>
+    )
+}
+
+export default Home;
