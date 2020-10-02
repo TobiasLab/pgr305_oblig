2b13b704d873e03a6d059b97338e8ca34053247b
TobiasLab <tobias.labidi@gmail.com>
2020-10-02 17:46:35 +0200

    you can now create new projects with a project owner on the same object

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const Projects = () => {
 
-    let Projects = [
+    const [projects, setNewProject] = useState([
         {
             Name: "Microsoft Project",
             Owner: "Christian"
@@ -29,10 +29,34 @@ const Projects = () => {
             Name: "BenQ Project",
             Owner: "Edwin"
         }
-    ];
+    ]);
+
+    const [nameInput, setNameInput] = useState();
+    const [nameValue, setNameValue] = useState();
+    const [ownerInput, setOwnerInput] = useState();
+    const [ownerValue, setOwnerValue] = useState();
+
+
+    const handleChangeName = (e) => {
+        setNameInput(e.target.value)
+        setNameValue(e.target.name)
+    }
+
+    const handleChangeOwner = (e) => {
+        setOwnerInput(e.target.value)
+        setOwnerValue(e.target.name)
+    }
+
+    const handleClick = () => {
+        const name = nameValue;
+        const nameValues = nameInput;
+        const owner = ownerValue
+        const ownerValues = ownerInput;
+        setNewProject(prevState => [...prevState, { [name]: nameValues, [owner]: ownerValues }]);
+    }
 
     let getProjects = () => {
-        return Projects.map((project, i) => {
+        return projects.map((project, i) => {
             return (
                 <Col key={`e-${i}`} xl={12}>
                     <nav>
@@ -54,6 +78,27 @@ const Projects = () => {
                 <h2>Current Project</h2>
             </Col>
             {getProjects()}
+            <Col>
+                <section>
+                    <input
+                        type="text"
+                        placeholder="Enter new project name"
+                        name="Name"
+                        onChange={handleChangeName}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Enter new project owner"
+                        name="Owner"
+                        onChange={handleChangeOwner}
+                    />
+                    <input
+                        type="button"
+                        onClick={handleClick}
+                        value="Add new projects"
+                    />
+                </section>
+            </Col>
         </Row>
     );
 }
